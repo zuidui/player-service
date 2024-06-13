10d6bed770cc29e288821bfaad2eaef0fc17d653
Manu <manloralm@outlook.com>
2024-06-13 14:04:59 -0600

    Update config.py

--- app/src/utils/config.py
+++ app/src/utils/config.py
@@ -9,31 +9,31 @@ load_dotenv(os.path.join(os.path.dirname(__file__), "../../.env"))
 
 class Settings(BaseSettings):
     DEBUG: bool
-    DEBUG_PORT: int
+    DEBUG_PORT: str
     LOG_LEVEL: str
     DOCKERHUB_USERNAME: str
     IMAGE_NAME: str
     IMAGE_VERSION: str
     APP_MODULE: str
-    APP_PORT: int
+    APP_PORT: str
     APP_HOST: str
     APP_DESCRIPTION: str
     API_PREFIX: str
     DEPENDENCIES: str
     DB_HOST: str
-    DB_PORT: int
+    DB_PORT: str
     DB_HOST_PORT: str
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
     API_GATEWAY_HOST: str
-    API_GATEWAY_PORT: int
+    API_GATEWAY_PORT: str
     BROKER_HOST: str
-    BROKER_PORT: int
-    BROKER_HEARTBEAT: int
-    BROKER_CONNECTION_ATTEMPTS: int
-    BROKER_CONNECTION_TIMEOUT: int
-    BROKER_ATTEMPT_DELAY: int
+    BROKER_PORT: str
+    BROKER_HEARTBEAT: str
+    BROKER_CONNECTION_ATTEMPTS: str
+    BROKER_CONNECTION_TIMEOUT: str
+    BROKER_ATTEMPT_DELAY: str
     QUEUE_NAME: str
 
     @property
